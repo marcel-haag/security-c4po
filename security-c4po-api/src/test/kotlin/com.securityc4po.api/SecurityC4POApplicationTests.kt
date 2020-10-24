906f1a170577b33ee93056628965fb385694b52b
Marcel Haag <marcel.haag@novatec-gmbh.de>
2020-10-24 16:25:05 +0200

    Created Initial SpringBoot App with OWASP Check and SpotBug

---
+++ security-c4po-api/src/test/kotlin/com.securityc4po.api/SecurityC4POApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.securityc4po.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityC4POApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
