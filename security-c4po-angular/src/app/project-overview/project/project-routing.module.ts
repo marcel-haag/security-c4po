a155832ad883662bd15891ea015af54f169415c3
Marcel Haag <marcelhaag1999@gmail.com>
2022-03-25 12:48:31 +0100

    fix: dialog overlay after lazy loading child component

--- security-c4po-angular/src/app/project-overview/project/project-routing.module.ts
+++ security-c4po-angular/src/app/project-overview/project/project-routing.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProjectComponent} from './project.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: ProjectComponent
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-export class ProjectRoutingModule { }
+export class ProjectRoutingModule {
+}
--- security-c4po-angular/src/shared/modules/project-dialog/project-dialog.module.ts
+++ security-c4po-angular/src/shared/modules/project-dialog/project-dialog.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {ProjectDialogComponent} from '@shared/modules/project-dialog/project-dialog.component';
 import {NbButtonModule, NbCardModule, NbDialogService, NbFormFieldModule, NbInputModule} from '@nebular/theme';
 import {FlexLayoutModule} from '@angular/flex-layout';
@@ -17,12 +17,12 @@ import {ProjectDialogService} from '@shared/modules/project-dialog/service/proje
     CommonModule,
     NbCardModule,
     NbButtonModule,
+    NbFormFieldModule,
+    NbInputModule,
     FlexLayoutModule,
     FontAwesomeModule,
     TranslateModule,
-    ReactiveFormsModule,
-    NbFormFieldModule,
-    NbInputModule,
+    ReactiveFormsModule
   ],
   providers: [
     DialogService,
@@ -33,4 +33,5 @@ import {ProjectDialogService} from '@shared/modules/project-dialog/service/proje
     ProjectDialogComponent
   ]
 })
-export class ProjectDialogModule { }
+export class ProjectDialogModule {
+}
