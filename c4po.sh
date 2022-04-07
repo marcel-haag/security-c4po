e27d6005db4086a711340f711c9e5967aed371be
nsm <nsm@novatec-gmbh.de>
2022-04-07 14:12:30 +0200

    feat: start C4PO with docker-compose

--- c4po.sh
+++ c4po.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 baseDir=$(pwd)
-
-composeDir=$baseDir"/security-c4po-cfg"
-keycloakVolume="security-c4po-cfg/volumes/keycloak/data/*"
-mongoVolume="security-c4po-cfg/volumes/mongodb/data/*"
-
-composeKeycloak=$baseDir"/security-c4po-cfg/kc/docker-compose.keycloak.yml"
-composeDatabase=$baseDir"/security-c4po-cfg/mongodb/docker-compose.mongodb.yml"
-composeFrontend=$baseDir"/security-c4po-cfg/frontend/docker-compose.frontend.yml"
-composeBackend=$baseDir"/security-c4po-cfg/backend/docker-compose.backend.yml"
-
 compose=$baseDir"/security-c4po-cfg/docker-compose.yml"
 
 echo -e "
@@ -24,24 +14,21 @@ ______| |______ |_____  |_____| |    \_ __|__    |       |         _/_/_/     _/
 
 echo "-------------CLEAN UP Container---------------"
 echo -e "\n"
-rm -r ${keycloakVolume}
 docker rm -f c4po-keycloak
-docker rm -f c4po-keycloak-postgres
-docker rm -f c4po-db
+#docker rm -f c4po-db     ### toggle to clear database with every start ###
 docker rm -f c4po-api
 docker rm -f c4po-angular
 echo -e "\n"
+
 echo "-----------------Start Build------------------"
 echo -e "\n"
 echo " - Backend: "
-docker-compose -f ${composeBackend} build
+docker-compose -f ${compose} build c4po-api
 echo -e "\n"
 echo " - Frontend: "
-docker-compose -f ${composeFrontend} build
+docker-compose -f ${compose} build c4po-angular
 echo -e "\n"
-# docker-compose -f ${compose} up
 
 echo "------------Start Docker Container------------"
 echo -e "\n"
-docker-compose -f ${composeKeycloak} -f ${composeDatabase} -f ${composeBackend} -f ${composeFrontend} up
-# docker-compose -f ${compose} up
+docker-compose -f ${compose} up
--- security-c4po-api/src/main/kotlin/com/securityc4po/api/configuration/security/WebSecurityConfiguration.kt
+++ security-c4po-api/src/main/kotlin/com/securityc4po/api/configuration/security/WebSecurityConfiguration.kt
@@ -1,12 +1,20 @@
 package com.securityc4po.api.configuration.security
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.core.OAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.jwt.JwtValidators
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.web.cors.CorsConfiguration
 
@@ -15,6 +23,12 @@ import org.springframework.web.cors.CorsConfiguration
 @Configuration
 @ComponentScan
 class WebSecurityConfiguration(private val userAccountDetailsService: UserAccountDetailsService) {
+
+    @Value("\${external.issuer-uri}")
+    var externalIssuerUri: String? = null
+
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    var internalIssuerUri: String? = null
 
     @Bean
     fun setSecurityWebFilterChains(http: ServerHttpSecurity): SecurityWebFilterChain {
@@ -28,24 +42,33 @@ class WebSecurityConfiguration(private val userAccountDetailsService: UserAccoun
                 this.addAllowedMethod(HttpMethod.PUT)
             }
         }
-                .and()
-                .csrf()
-                .disable()
-                .authorizeExchange()
-                .pathMatchers(HttpMethod.GET, "/v1/projects/**").authenticated()
-                .pathMatchers(HttpMethod.GET, "/v1/pentests/**").authenticated()
-                .pathMatchers("/actuator/**").permitAll()
-                .pathMatchers("/docs/SecurityC4PO.html").permitAll()
-                .anyExchange().authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(appuserJwtAuthenticationConverter())
+            .and()
+            .csrf()
+            .disable()
+            .authorizeExchange()
+            .pathMatchers(HttpMethod.GET, "/v1/projects/**").authenticated()
+            .pathMatchers(HttpMethod.GET, "/v1/pentests/**").authenticated()
+            .pathMatchers("/actuator/**").permitAll()
+            .pathMatchers("/docs/SecurityC4PO.html").permitAll()
+            .anyExchange().authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(appuserJwtAuthenticationConverter())
         return http.build()
     }
 
     @Bean
     fun appuserJwtAuthenticationConverter(): AppuserJwtAuthConverter {
         return AppuserJwtAuthConverter(userAccountDetailsService)
+    }
+
+    @Bean
+    @Profile("COMPOSE")
+    fun jwtDecoder(): ReactiveJwtDecoder {
+        val jwtDecoder = ReactiveJwtDecoders.fromIssuerLocation(internalIssuerUri) as NimbusReactiveJwtDecoder
+        val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer(externalIssuerUri)
+        jwtDecoder.setJwtValidator(withIssuer)
+        return jwtDecoder
     }
 }
