7c1784584e299b2b06cb1a1da9f81d621ede9b0e
nsm <nsm@novatec-gmbh.de>
2022-05-30 18:16:50 +0200

    fix: fix keycloak connection

---
+++ security-c4po-angular/src/environments/environment.compose.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+
+  // keycloak
+  keycloakURL: 'http://localhost:8888/auth',
+  keycloakrealm: 'c4po_realm_local',
+  keycloakclientId: 'c4po_local',
+
+  // backend service
+  apiEndpoint: 'http://localhost:8443',
+};
